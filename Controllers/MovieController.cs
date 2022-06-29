02052118f1f4075ee0b6993f66cf287bda67985a
zeMontreuilMITT <zacharie.montreuil@mitt.ca>
2022-06-29 10:20:03 -0500

    Final solution for Movie Controller

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -10,6 +10,7 @@ namespace SD_115_W22SD_Code_Examples_MVC.Controllers
             // only show films from 2010 and later
             List<Film> newFilms = Database.Films.Where(iteratedFilm => iteratedFilm.Year > 2015).ToList();
 
+            ViewBag.PageTitle = "All Films";
             return View(newFilms);
         }
 
@@ -37,6 +38,57 @@ namespace SD_115_W22SD_Code_Examples_MVC.Controllers
             {
                 return RedirectToAction("Error");
             }
+        }
+
+        public IActionResult MostExpensive()
+        {
+            /*
+            Film expensiveFilm;
+
+            double greatestBudget = Database.Films.Max(f => f.Budget);
+
+            expensiveFilm = Database.Films.First(f => f.Budget == greatestBudget);
+
+            return View("Details", expensiveFilm); */
+
+            List<Film> filmsByBudget = Database.Films.OrderByDescending(f => f.Budget).ToList();
+
+            ViewBag.PageTitle = "Most Expensive Films";
+            return View("Index", filmsByBudget);
+        }
+
+        public IActionResult LeastExpensive()
+        {
+            List<Film> filmsByBudget = Database.Films.OrderBy(f => f.Budget).ToList();
+
+            ViewBag.PageTitle = "Least Expensive Films";
+            return View("Index", filmsByBudget);
+        }
+
+        public IActionResult AlphabeticFilms()
+        {
+            List<Film> alphabeticFilms = Database.Films.OrderBy(f => f.Title).ToList();
+
+            ViewBag.PageTitle = "Philms by Alphabetic Order";
+            return View("Index", alphabeticFilms);
+        }
+
+        public IActionResult BudgetRange(long? maxBudget = 0, long? minBudget = 0)
+        {
+            if(maxBudget != null && minBudget != null)
+            {
+                List<Film> filmsInRange = Database.Films.Where(f => f.Budget >= minBudget && f.Budget <= maxBudget).ToList();
+
+                if(filmsInRange.Count <= 0)
+                {
+                    ViewBag.Message = "Didn't find any!";
+                }
+
+                ViewBag.PageTitle = $"Films between ${minBudget} and ${maxBudget}";
+                return View("Index", filmsInRange);
+            }
+
+            return RedirectToAction("Error");
         }
 
         public IActionResult Error(string message)
