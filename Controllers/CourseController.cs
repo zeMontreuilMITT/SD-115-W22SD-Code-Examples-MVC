653cc4b35d162c90447fb5480eee61ec6ca0d026
zeMontreuilMITT <zacharie.montreuil@mitt.ca>
2022-06-30 08:44:21 -0500

    Example of using SelectList items with ViewModel

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using SD_115_W22SD_Code_Examples_MVC.Models;
 
 namespace SD_115_W22SD_Code_Examples_MVC.Controllers
@@ -6,8 +7,25 @@ namespace SD_115_W22SD_Code_Examples_MVC.Controllers
     public class CourseController: Controller
     {
         public IActionResult Index()
-        { 
+        {
+
             return View(Database.Courses);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int? Id)
+        {
+            CourseSelectViewModel vm;
+            if(Id == null)
+            {
+                vm = new CourseSelectViewModel(Database.Courses);
+            } else
+            {
+                Course foundCourse = Database.Courses.First(c => c.Id == Id);
+                vm = new CourseSelectViewModel(foundCourse, Database.Courses);
+            }
+
+            return View(vm);
         }
 
         [HttpGet]
---
+++ Models/CourseSelectViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SD_115_W22SD_Code_Examples_MVC.Models
+{
+    public class CourseSelectViewModel
+    {
+        public Course? Course { get; set; }
+        public List<SelectListItem> SelectCourses { get; set; } = new List<SelectListItem>();
+
+        public CourseSelectViewModel(Course course, ICollection<Course> listCourses)
+        {
+            Course = course;
+            
+            foreach(Course c in listCourses)
+            {
+                SelectCourses.Add(new SelectListItem { Value = c.Id.ToString(), Text = c.Title });
+            }
+        }
+
+        public CourseSelectViewModel(ICollection<Course> listCourses)
+        {
+            foreach (Course c in listCourses)
+            {
+                SelectCourses.Add(new SelectListItem { Value = c.Id.ToString(), Text = c.Title });
+            }
+        }
+    }
+}
